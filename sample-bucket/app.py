737ff6dbcd79dd2f04605a60cac5fe44496913cd
Christian Chostak <cchostak@chaplau.com>
2022-05-26 19:36:00 +0100

    Made the stacks env agnostic

--- sample-bucket/app.py
+++ sample-bucket/app.py
@@ -7,11 +7,17 @@ from queue.queue_stack import QueueStack
 from key.key_stack import KeyStack
 from application.app_stack import AppStack
 
+the_region = os.getenv("REGION", "us-east-1")
+the_acc = os.getenv("ACCOUNT", "")
+task_account = int(os.getenv("TASK_DESIRED_COUNT", 0))
+
+env = cdk.Environment(account=the_acc, region=the_region)
+
 app = cdk.App()
 
 # Place my stacks
 key_stack = KeyStack(app, "KeyStack") # key_stack.encryption_key
 queue_stack = QueueStack(app, "QueueStack", key=key_stack.encryption_key)
-app_stack = AppStack(app, "AppStack")
+app_stack = AppStack(app, "AppStackEu", task_count=task_account, env=env)
 
 app.synth()
--- sample-bucket/application/app_stack.py
+++ sample-bucket/application/app_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Duration,
     Stack,
+    CfnParameter,
     aws_sqs as sqs,
     aws_kms as kms,
     aws_ecs as ecs,
@@ -10,12 +11,15 @@ from constructs import Construct
 
 class AppStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, task_count: int, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        task_memory_limit = CfnParameter(self, "TaskMemoryLimit", type="Number",
+            description="The task memory limit")
+
         load_balanced_fargate_service = ecsp.ApplicationLoadBalancedFargateService(self, "Service",
-            memory_limit_mib=1024,
-            desired_count=1,
+            memory_limit_mib=task_memory_limit.value_as_number,
+            desired_count=task_count,
             cpu=512,
             task_image_options=ecsp.ApplicationLoadBalancedTaskImageOptions(
                 image=ecs.ContainerImage.from_registry("httpd")
